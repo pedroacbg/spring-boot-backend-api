de4291ad35597fa17ab60c1deb60e21960deb12b
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2022-04-10 19:38:59 -0300

    Testing first entity

--- src/main/java/com/pedroanjos/cursomc/resources/CategoryResource.java
+++ src/main/java/com/pedroanjos/cursomc/resources/CategoryResource.java
@@ -1,16 +1,28 @@
 package com.pedroanjos.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pedroanjos.cursomc.entities.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
 	@GetMapping
-	public String listar() {
-		return "REST on!";
+	public List<Category> list() {
+		Category cat1 = new Category(1L, "Informática");
+		Category cat2 = new Category(2L, "Escritório");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 	
 }
