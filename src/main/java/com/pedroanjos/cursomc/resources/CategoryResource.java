fce3956cb2d987a6f12e616e7a159b7395df78b5
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2022-04-10 19:20:07 -0300

    REST testing

---
+++ src/main/java/com/pedroanjos/cursomc/resources/CategoryResource.java
@@ -0,0 +1,16 @@
+package com.pedroanjos.cursomc.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+	@GetMapping
+	public String listar() {
+		return "REST on!";
+	}
+	
+}
