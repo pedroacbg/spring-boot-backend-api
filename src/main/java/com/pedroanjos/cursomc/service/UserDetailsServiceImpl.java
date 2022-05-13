a4ba68cdf4a9dd3218b70e39563bfc2ccf40a67c
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2022-05-13 19:28:11 -0300

    Implementing authentication and generating JWT token

---
+++ src/main/java/com/pedroanjos/cursomc/service/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.pedroanjos.cursomc.service;
+
+import com.pedroanjos.cursomc.entities.Client;
+import com.pedroanjos.cursomc.repositories.ClientRepository;
+import com.pedroanjos.cursomc.security.UserSS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Client client = clientRepository.findByEmail(email);
+        if(client == null){
+            throw new UsernameNotFoundException(email);
+        }
+        return new UserSS(client.getId(), client.getEmail(), client.getPassword(), client.getProfiles());
+    }
+}
