388a8a49827f07f2400f08220f6ffe145caaa5ce
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2022-05-13 09:14:03 -0300

    Dev profile created

--- src/main/java/com/pedroanjos/cursomc/CursomcApplication.java
+++ src/main/java/com/pedroanjos/cursomc/CursomcApplication.java
@@ -13,14 +13,11 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class CursomcApplication implements CommandLineRunner {
+public class CursomcApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-	}
 
 }
