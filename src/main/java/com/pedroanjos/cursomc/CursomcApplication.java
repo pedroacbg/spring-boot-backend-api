1e72bf1159fe72aca0d85d9e699ac2c1de25c819
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2022-05-14 14:45:51 -0300

    Saving first file on S3

--- src/main/java/com/pedroanjos/cursomc/CursomcApplication.java
+++ src/main/java/com/pedroanjos/cursomc/CursomcApplication.java
@@ -4,6 +4,7 @@ import com.pedroanjos.cursomc.entities.*;
 import com.pedroanjos.cursomc.entities.enums.PaymentStatus;
 import com.pedroanjos.cursomc.entities.enums.TypeClient;
 import com.pedroanjos.cursomc.repositories.*;
+import com.pedroanjos.cursomc.services.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,11 +14,18 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class CursomcApplication {
-	
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private S3Service service;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 
+	@Override
+	public void run(String... args) throws Exception {
+		service.uploadFile("E:\\Jafari Adisa.png");
+	}
 }
